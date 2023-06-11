527fae4a4861a1f87e8291f3226890ad08b3406c
Raymond Li <hi@raymond.li>
2023-06-11 14:28:27 -0400

    Implement TTS

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,9 @@
  */
 
 // GCP Text-to-speech API
-const UPSTREAM_ENDPOINT = "https://texttospeech.googleapis.com/v1/";
+const UPSTREAM_TTS_ENDPOINT = "https://texttospeech.googleapis.com/v1/";
+// GCP Translate API
+const UPSTREAM_TRANSLATE_ENDPOINT = "https://translation.googleapis.com/language/translate/v2/";
 
 interface Env {
 	ORIGIN: string;
@@ -39,11 +41,11 @@ export default {
 			if (url.search) { // ensure query string (key, languageCode) exists
 				const params: URLSearchParams = new URLSearchParams(url.search);
 				if (params.has("languageCode")) {
-					response = await fetch(UPSTREAM_ENDPOINT + "voices?languageCode=" + params.get("languageCode")
+					response = await fetch(UPSTREAM_TTS_ENDPOINT + "voices?languageCode=" + params.get("languageCode")
 						+ "&key=" + env.GCP_API_KEY, {headers: {"Referer": env.ORIGIN}}
 					);
 				} else {
-					response = await fetch(UPSTREAM_ENDPOINT + "voices?key=" + env.GCP_API_KEY,
+					response = await fetch(UPSTREAM_TTS_ENDPOINT + "voices?key=" + env.GCP_API_KEY,
 						{headers: {"Referer": env.ORIGIN}});
 				}
 			}
@@ -53,18 +55,48 @@ export default {
 				return new Response(null, {status: 405}); // Method Not Allowed
 			}
 			if (url.search) { // ensure query string (key) exists
-				// Reject request if json body is longer than 500 characters
 				const body: JSON = await request.json();
+				// Validate request body
 				// @ts-ignore
-				if (body["input"]["text"].length > 500) {
-					return new Response(null, {status: 413}); // Payload Too Large
-				}
-				response = await fetch(UPSTREAM_ENDPOINT + "text:synthesize?key=" + env.GCP_API_KEY, {
-						method: "POST",
-						headers: {"Referer": env.ORIGIN},
-						body: JSON.stringify(body),
+				if (body["input"]["text"] && body["voice"]["languageCode"] && body["voice"]["name"] && body["audioConfig"]["audioEncoding"]) {
+					// Reject request if json body is longer than 500 characters
+					// @ts-ignore
+					if (body["input"]["text"].length > 500) {
+						return new Response(null, {status: 413}); // Payload Too Large
 					}
-				);
+					response = await fetch(UPSTREAM_TTS_ENDPOINT + "text:synthesize?key=" + env.GCP_API_KEY, {
+							method: "POST",
+							headers: {"Referer": env.ORIGIN},
+							// @ts-ignore
+							body: JSON.stringify(body),
+						}
+					);
+				}
+			}
+		}
+		if (url.pathname === "/translate") {
+			if (request.method !== "POST") {
+				return new Response(null, {status: 405}); // Method Not Allowed
+			}
+			if (url.search) { // ensure query string (key) exists
+				const params: URLSearchParams = new URLSearchParams(url.search);
+				if (params.has("q") && params.has("target")) { // required query params
+					// @ts-ignore
+					const query: string = params.get("q");
+
+					// Reject request if query is longer than 500 characters
+					if (query.length > 500) {
+						return new Response(null, {status: 413}); // Payload Too Large
+					}
+					response = await fetch(UPSTREAM_TRANSLATE_ENDPOINT + "?q=" + query +
+						(params.has("target") ? "&target=" + params.get("target") : "") +
+						(params.has("source") ? "&source=" + params.get("source") : "") +
+						"&key=" + env.GCP_API_KEY, {
+							method: "POST",
+							headers: {"Referer": env.ORIGIN},
+						}
+					);
+				}
 			}
 		}
 		if (!response) {
